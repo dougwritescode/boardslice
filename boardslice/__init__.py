19766b9e0f84ca687c847e2f353bdc622c8e4ab2
Doug Walter <dougwritescode@gmail.com>
2017-02-24 14:50:00 -0500

    added linejoin output option

--- boardslice/__init__.py
+++ boardslice/__init__.py
@@ -10,7 +10,8 @@ enums = parser.add_mutually_exclusive_group()
 enums.add_argument('-e', '--enum', metavar='INT', dest='e', nargs='?', help='Number the lines on output, starting with argument value. Defaults to 1 without argument.', const=1, action='store')
 enums.add_argument('-d', '--enumdown', metavar='INT', dest='d', nargs='?', help='Number the lines on output, going down, starting with argument value. Defaults to the number of lines without argument.', const='num', action='store')
 parser.add_argument('-s', '--splitdelimiter', metavar='CHAR[S]', dest='s', nargs='?', type=str, help='Character/s to split input on, defaults to \'\\t\'.', default='\t', const=None, action='store')
-parser.add_argument('-j', '--joindelimiter', metavar='CHAR[S]', dest='j', type=str, help='Character/s to separate output, defaults to \'\\t\'..', default='\t', action='store')
+parser.add_argument('-j', '--joindelimiter', metavar='CHAR[S]', dest='j', type=str, help='Character/s to separate output, defaults to \'\\t\'.', default='\t', action='store')
+parser.add_argument('-l', '--linejoindelimiter', metavar='CHAR[S]', dest='lj', type=str, help='Character/s to join lines on, defaults to \'\\n\'.', default='\n', action='store')
 parser.add_argument('-r', '--reverse', dest='r', help='Reverse the order of the lines on output.', action='store_true')
 parser.add_argument('-rl', '--reverselines', dest='l', help='Reverse each line on output.', action='store_true')
 
@@ -30,7 +31,7 @@ def main():
 	except IndexError:
 		print "Your input is not all the same length!"
 		print "Check the formatting!"
-	lines = [args.j.join(l) for l in c]
+	lines = [args.j.join(a) for a in c]
 	ll = len(lines)
 	if args.r:
 		lines = lines[::-1]
@@ -40,7 +41,7 @@ def main():
 		lines = [str(args.e + x) + args.j + lines[x] for x in range(ll)]
 	if args.d != None:
 		lines = [str(args.d - x) + args.j + lines[x] for x in range(ll)]
-	copy('\n'.join(lines))
+	copy(args.lj.join(lines))
 		
 if __name__=="__main__":
 	main()
